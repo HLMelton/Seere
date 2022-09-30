00978b7feb9b2fe8a0f3ed5f64f776efeb584d49
Hunter L. Melton <hunterlmelton@gmail.com>
2022-09-30 11:46:19 -0700

    PRicing to home shift

--- components/Home.vue
+++ components/Home.vue
@@ -6,40 +6,7 @@
 
 <script>
 
-const axios = require('axios')
-// TODO: Figure out why Cheerio is mad at me. 
-// const cheerion = require('cheerio')
-
 export default {
-  data(){
-    return{
-        latestPrices: [],
-    }
-  },
   name: 'Home',
-  methods:{
-    getPrices(){
-        // change this to the object db reference after initial test
-        let url = 'https://www.target.com/p/2-reduced-fat-milk-1gal-good-38-gather-8482/-/A-13276204';
-
-        axios({
-          method: 'get',
-          url: url,
-        })
-        .then(function (response){
-          let html = response.data;
-
-          // let $ = cheerio.load(html)
-          console.log('pre')
-          // TODO: Find why the content of the html is returning as the content of the current page rather than the page requested. 
-          console.log(html)
-          console.log('post')
-        })
-
-    }
-  },
-  created(){
-    this.getPrices();
-  }
 }
 </script>
--- components/Pricing.vue
+++ components/Pricing.vue
@@ -5,7 +5,43 @@
 </template>
 
 <script>
+
+const axios = require('axios')
+// TODO: Figure out why Cheerio is mad at me. 
+//       Though we need to get the data from 
+//       the page before we worry about handling it. 
+
+// const cheerio = require('cheerio')
+
 export default {
-  name: 'Pricing'
-}
+  data(){
+
+    let res = await axios.get('https://www.target.com/p/2-reduced-fat-milk-1gal-good-38-gather-8482/-/A-13276204')
+    .then(({ data }) => console.log(data))
+    return{
+        latestPrices: [],
+    }
+  },
+  name: 'Pricing',
+  methods:{
+    getPrices(){
+        // change this to the object db reference after initial test
+        let url = 'https://www.target.com/p/2-reduced-fat-milk-1gal-good-38-gather-8482/-/A-13276204';
+
+        axios({
+          method: 'get',
+          url: url,
+        })
+        .then(function (response){
+          let html = response.data;
+
+          // let $ = cheerio.load(html)
+          console.log('pre')
+          // TODO: Find why the content of the html is returning as the content of the current page rather than the page requested. 
+          console.log(html)
+          console.log('post')
+        })
+      }
+    },
+ }
 </script>
