5838c0cad35dfc123f51b6a2974fe2db47d4ca91
Hunter L. Melton <hunterlmelton@gmail.com>
2023-04-28 10:00:53 -0700

    store fix p2

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <script lang="ts">
 import BingoInput from './components/BingoInput.vue'
+import BingoDisplay from './components/BingoDisplay.vue';
 
 export default {
   components:{
     BingoInput,
+    BingoDisplay,
   },
 }
 </script>
 
 <template>
   <div class="bg-slate-600 h-screen w-screen pt-10">
-    <div class="bg-slate-300 h-[80%] w-[80%] mx-auto flex space-x-3 pt-3">
-      <div class="bg-white">
-        <TextInput />
+    <div class="bg-slate-300 h-[80%] w-[80%] mx-auto flex space-x-3 pt-3 pb-3 px-3 rounded-md">
+    <!-- TODO: Fix CSS Classes to be an abstract style for the container -->
+      <div class="bg-white flex-1 rounded-lg">
+        <BingoInput />
       </div>
-      <div class="bg-white">Score Card Output Area</div>
+      <div class="bg-white flex-1 rounded-lg">
+        <BingoDisplay />
+      </div>
     </div>
   </div>
 </template>
