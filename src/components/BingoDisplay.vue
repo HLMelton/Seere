6107a0c87610d1ebadcf1ec0f2de5304ae0b9566
HLMelton <hunterlmelton@gmail.com>
2023-04-29 00:50:09 -0700

    custom styling and grid layout

--- src/components/BingoDisplay.vue
+++ src/components/BingoDisplay.vue
@@ -1,9 +1,13 @@
 <script lang="ts">
 import { useLibraryStore } from '../store/library';
+import { ref } from 'vue';
 
 export default {
     setup() {
         const store = useLibraryStore();
+        const testArray = ref([])
+        
+        return testArray
     },
     methods:{
 
@@ -12,9 +16,12 @@ export default {
 </script>
 
 <template>
-    <h1 class="text-center">
-        Display The Card Here 
-    </h1>
+    <div class="grid grid-cols-5 space-y-1 space-x-1 p-5">
+        <!-- add randomization of each vslot and map each randomized element in the array to each slot. -->
+        <!-- Also abstract the classes into custom card components -->
+        <li v-for="card in testArray"> {{ card }}</li>
+
+    </div>
     <h1 class="text-center">
         Put the Generate Button Here 
     </h1>
