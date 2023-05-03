44261227a8efbef046f3c39e8c6f9151a8556a12
HLMelton <hunterlmelton@gmail.com>
2023-05-03 15:59:03 -0700

    spacings and stuff

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,16 @@ export default {
 <template>
   <h1 class="text-white absolute w-screen text-center pt-1 text-5xl">Seere</h1>
   <div class="bg-slate-600 h-screen w-screen pt-16">
-    <div class="bg-[#B4CDED] h-4/6 w-10/12 mx-auto flex space-x-3 pt-3 pb-3 px-3 rounded-md">
-    <!-- TODO: Fix CSS Classes to be an abstract style for the container -->
+    <div class="bg-[#B4CDED] h-4/6 w-10/12 mx-auto flex flex-row space-x-3 pt-3 pb-3 px-3 rounded-md">
+
       <div class="bg-[#F0F4EF] flex-1 rounded-lg">
         <BingoInput />
       </div>
+
       <div class="bg-[#F0F4EF] flex-1 rounded-lg">
         <BingoDisplay />
       </div>
+      
     </div>
   </div>
 </template>
